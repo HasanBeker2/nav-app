635f199c2a0364ad70e4d2bec609219db1cd64d4
hasanbeker22 <hasanbeker22@gmail.com>
2023-01-26 01:43:15 +0100

    New App nav-app is loaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Homepage from "./Homepage"
+import AboutMe from "./AboutMe"
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav className="nav">
+        <Link to="/" className="nav- item" >Homepage</Link>
+        <Link to="/about-me" className="nav-item">AboutMe</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
+    </div >
   );
 }
 
