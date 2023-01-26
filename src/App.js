16be8da84afd2125f987057b3ad21bfc3b4bc1a9
hasanbeker22 <hasanbeker22@gmail.com>
2023-01-26 02:10:36 +0100

    Contact and new App.css file added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Homepage from "./Homepage"
 import AboutMe from "./AboutMe"
+import Contact from "./Contact";
+import "./App.css"
 import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <nav className="nav">
-        <Link to="/" className="nav- item" >Homepage</Link>
+        <Link to="/" className="nav-item">Homepage</Link>
         <Link to="/about-me" className="nav-item">AboutMe</Link>
+        <Link to="/contact" className="nav-item">Contact</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div >
   );
